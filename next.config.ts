7e6b8c5a6e9610ba5095dabdc1a97ee0e1951c2c
Austin Lu <austinl53@nycstudents.net>
2026-06-18 17:35:13 -0400

    Pass env vars through Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    POWABASE_URL: process.env.POWABASE_URL!,
+    POWABASE_KEY: process.env.POWABASE_KEY!,
+  },
 };
 
 export default nextConfig;
